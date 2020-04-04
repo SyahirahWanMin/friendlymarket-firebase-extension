fe84726f3c4250c1fab4b9685d442c1a0ec4ad87
SyahirahWanMin <b031610190@gmail.com>
2020-04-05 05:01:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-n6anga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-n6anga)
---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+var firebaseConfig = {
+    apiKey: "AIzaSyAdZuMCIcTDchfjfaZIn1styDGgDyrZy34",
+    authDomain: "friendlymarket-c4d07.firebaseapp.com",
+    databaseURL: "https://friendlymarket-c4d07.firebaseio.com",
+    projectId: "friendlymarket-c4d07",
+    storageBucket: "friendlymarket-c4d07.appspot.com",
+    messagingSenderId: "594235799324",
+    appId: "1:594235799324:web:062ed867a29f4c1eebecd9",
+    measurementId: "G-F1NEHMYRJJ"
+  };
+
+export default firebaseConfig;
